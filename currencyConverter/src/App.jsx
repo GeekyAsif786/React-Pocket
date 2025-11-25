9c2dbd8b23680e50ee9909ad4463cbb98001976f
Asifur Rahaman <asif@Asifurs-MacBook-Air.local>
2025-11-25 17:29:26 +0530

    modernized UI

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -69,7 +69,9 @@ function App() {
                                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-full bg-blue-600 text-white px-3 py-1 hover:bg-blue-700 hover:scale-110 transition-all duration-300 shadow-lg"
                                 onClick={swap}
                             >
-                                Swap
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                                </svg>
                             </button>
                         </div>
                         <div className="w-full mt-1 mb-4">
